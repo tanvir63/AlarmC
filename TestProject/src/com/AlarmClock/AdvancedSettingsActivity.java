5832b5f5a99b910545cca3bfb5a2af817e923bac
tanvir63 <tanvir63@ymail.com>
2016-01-05 21:21:14 -0500

    Shared Preference

--- TestProject/src/com/AlarmClock/AdvancedSettingsActivity.java
+++ TestProject/src/com/AlarmClock/AdvancedSettingsActivity.java
@@ -1,5 +1,8 @@
 package com.AlarmClock;
 
+import com.utils.Constants;
+import com.utils.Utils;
+
 import android.app.Activity;
 import android.app.Dialog;
 import android.app.DialogFragment;
@@ -9,25 +12,52 @@ import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ImageView;
 
-public class AdvancedSettingsActivity extends Activity implements
-		OnClickListener {
-
+public class AdvancedSettingsActivity extends Activity {
+	private CheckBox isFlashLightOnCheckBoxView;
+	private CheckBox showAlarmNotificationCheckBoxView;
+	private CheckBox IsAutoSnoozeOnCheckBoxView;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.advanced_settings);
+		
+		boolean isFlashLightOn=Utils.getBoolValue(this,Constants.FLASH_LIGHT,false);
+		boolean showAlarmNotification= Utils.getBoolValue(this,Constants.SHOW_ALARM_NOTIFICATION,false);
+		boolean IsAutoSnoozeOn= Utils.getBoolValue(this,Constants.IS_AUTO_SNOOZE_ON,false);
+		
+		isFlashLightOnCheckBoxView = (CheckBox) findViewById(R.id.isFlashLightOnCheckBox);
+		isFlashLightOnCheckBoxView.setChecked(isFlashLightOn);
+		showAlarmNotificationCheckBoxView = (CheckBox) findViewById(R.id.showAlarmNotificationCheckBox);
+		showAlarmNotificationCheckBoxView.setChecked(showAlarmNotification);
+		IsAutoSnoozeOnCheckBoxView = (CheckBox) findViewById(R.id.autoSnoozeLayoutCheckBox);
+		IsAutoSnoozeOnCheckBoxView.setChecked(IsAutoSnoozeOn);
 	}
 
 	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		// case R.id.saveButton:
-		// createUpdateClient();
-		// break;
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        isFlashLightOnCheckBoxView.setOnCheckedChangeListener(new CheckedChangeListener(this,Constants.FLASH_LIGHT));
+        showAlarmNotificationCheckBoxView.setOnCheckedChangeListener(new CheckedChangeListener(this,Constants.SHOW_ALARM_NOTIFICATION));
+        IsAutoSnoozeOnCheckBoxView.setOnCheckedChangeListener(new CheckedChangeListener(this,Constants.IS_AUTO_SNOOZE_ON));
+    }
+	
+	public class CheckedChangeListener implements CompoundButton.OnCheckedChangeListener {
+        String checkBoxName;
+        Context context;
+        public CheckedChangeListener(Context context,String checkBoxName) {
+            this.checkBoxName = checkBoxName;
+            this.context=context;
+        }
 
-		default:
-			break;
-		}
-	}
+        @Override
+        public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+        	Utils.putBoolValue(context,checkBoxName,isChecked);
+        }
+    }
 }
--- TestProject/src/com/utils/Constants.java
+++ TestProject/src/com/utils/Constants.java
@@ -6,4 +6,8 @@ public class Constants {
 	public static final int INSERT_OPERATION = 1;
 	public static final int UPDATE_OPERATION = 2;
 	public static final String KEY_ALARM_INFO = "alarm_info";
+	
+	public static final String FLASH_LIGHT = "Flash_Light";
+	public static final String SHOW_ALARM_NOTIFICATION = "ShowAlarmNotification";
+	public static final String IS_AUTO_SNOOZE_ON = "AutoSnooze";
 }
